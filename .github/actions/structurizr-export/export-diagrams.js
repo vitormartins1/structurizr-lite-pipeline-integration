6c535d59ff97c75a93f6087004b4a4bf23faf7e2
Vitor Martins <vitormartins.one@gmail.com>
2024-12-19 21:42:47 -0300

    Update export-diagrams.js

--- .github/actions/structurizr-export/export-diagrams.js
+++ .github/actions/structurizr-export/export-diagrams.js
@@ -13,8 +13,8 @@ const IMAGE_VIEW_TYPE = 'Image';
 // Constante para o diretório de saída
 const outputDir = '/home/runner/work/structurizr-pipeline-integration/structurizr-pipeline-integration/docs/diagrams/';
 
-if (process.argv.length < 4) {
-  console.log("Usage: <structurizrUrl> <png|svg> [username] [password]");
+if (process.argv.length < 2) {
+  console.log("Usage: <structurizrUrl> <png|svg>");
   process.exit(1);
 }
 
@@ -26,67 +26,93 @@ if (format !== PNG_FORMAT && format !== SVG_FORMAT) {
   process.exit(1);
 }
 
-let username;
-let password;
-
-if (process.argv.length > 4) {
-  username = process.argv[4];
-  password = process.argv[5];
-}
-
 (async () => {
   const browser = await puppeteer.launch({ ignoreHTTPSErrors: IGNORE_HTTPS_ERRORS, headless: HEADLESS });
   const page = await browser.newPage();
 
-  // Criar o diretório de saída se não existir
-  if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir, { recursive: true });
-  }
-
-  if (username && password) {
-    const parts = url.split('://');
-    const signinUrl = parts[0] + '://' + parts[1].substring(0, parts[1].indexOf('/')) + '/dashboard';
-    console.log(` - Signing in via ${signinUrl}`);
-
-    await page.goto(signinUrl, { waitUntil: 'networkidle2' });
-    await page.type('#username', username);
-    await page.type('#password', password);
-    await page.keyboard.press('Enter');
-    await page.waitForSelector('div#dashboard');
-  }
-
-  console.log(` - Opening ${url}`);
-  await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 }); // 60 segundos
-  await page.waitForFunction(
-    'structurizr.scripting.isDiagramRendered() === true',
-    { timeout: 60000 } // 60 segundos
-  );
+  // visit the diagrams page
+  console.log(" - Opening " + url);
+  await page.goto(url, { waitUntil: 'domcontentloaded' });
+  await page.waitForFunction('structurizr.scripting && structurizr.scripting.isDiagramRendered() === true');
   
-  const views = await page.evaluate(() => structurizr.scripting.getViews());
+  if (format === PNG_FORMAT) {
+    // add a function to the page to save the generated PNG images
+    await page.exposeFunction('savePNG', (content, filename) => {
+      console.log(" - " + filename);
+      content = content.replace(/^data:image\/png;base64,/, "");
+      fs.writeFile(filename, content, 'base64', function (err) {
+        if (err) throw err;
+      });
+      
+      actualNumberOfExports++;
+
+      if (actualNumberOfExports === expectedNumberOfExports) {
+        console.log(" - Finished");
+        browser.close();
+      }
+    });
+  }
+
+  // get the array of views
+  const views = await page.evaluate(() => {
+    return structurizr.scripting.getViews();
+  });
+
+  views.forEach(function(view) {
+    if (view.type === IMAGE_VIEW_TYPE) {
+      expectedNumberOfExports++; // diagram only
+    } else {
+      expectedNumberOfExports++; // diagram
+      expectedNumberOfExports++; // key
+    }
+  });
+
   console.log(" - Starting export");
+  // const diagramFilename = path.join(outputDir, `${view.key}.${format}`);
+  // const diagramKeyFilename = path.join(outputDir, `${view.key}-key.${format}`);
 
-  for (const view of views) {
-    const diagramFilename = path.join(outputDir, `${view.key}.${format}`);
-    const diagramKeyFilename = path.join(outputDir, `${view.key}-key.${format}`);
+  for (var i = 0; i < views.length; i++) {
+    const view = views[i];
 
-    await page.evaluate(view => structurizr.scripting.changeView(view.key), view);
+    await page.evaluate((view) => {
+      structurizr.scripting.changeView(view.key);
+    }, view);
+
     await page.waitForFunction('structurizr.scripting.isDiagramRendered() === true');
 
     if (format === SVG_FORMAT) {
-      const svgForDiagram = await page.evaluate(() =>
-        structurizr.scripting.exportCurrentDiagramToSVG({ includeMetadata: true })
-      );
-      fs.writeFileSync(diagramFilename, svgForDiagram, 'utf8');
-      console.log(` - Exported: ${diagramFilename}`);
+      const diagramFilename = outputDir + view.key + '.svg';
+      const diagramKeyFilename = outputDir + view.key + '-key.svg'
 
+      var svgForDiagram = await page.evaluate(() => {
+        return structurizr.scripting.exportCurrentDiagramToSVG({ includeMetadata: true });
+      });
+    
+      console.log(" - " + diagramFilename);
+      fs.writeFile(diagramFilename, svgForDiagram, function (err) {
+        if (err) throw err;
+      });
+      actualNumberOfExports++;
+    
       if (view.type !== IMAGE_VIEW_TYPE) {
-        const svgForKey = await page.evaluate(() => structurizr.scripting.exportCurrentDiagramKeyToSVG());
-        fs.writeFileSync(diagramKeyFilename, svgForKey, 'utf8');
-        console.log(` - Exported: ${diagramKeyFilename}`);
+        var svgForKey = await page.evaluate(() => {
+          return structurizr.scripting.exportCurrentDiagramKeyToSVG();
+        });
+      
+        console.log(" - " + diagramKeyFilename);
+        fs.writeFile(diagramKeyFilename, svgForKey, function (err) {
+          if (err) throw err;
+        });
+        actualNumberOfExports++;
       }
+
+      if (actualNumberOfExports === expectedNumberOfExports) {
+        console.log(" - Finished");
+        browser.close();
+      }    
     } else {
-      const diagramFilename = view.key + '.png';
-      const diagramKeyFilename = view.key + '-key.png'
+      const diagramFilename = outputDir + view.key + '.png';
+      const diagramKeyFilename = outputDir + view.key + '-key.png'
 
       page.evaluate((diagramFilename) => {
         structurizr.scripting.exportCurrentDiagramToPNG({ includeMetadata: true, crop: false }, function(png) {
